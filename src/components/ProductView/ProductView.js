78fe1b302ab239cbfce32943870a3c186baab440
Dakota Benger <dakota@bengerlaw.com>
2020-12-09 15:47:02 -0700

    finished project

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -6,12 +6,26 @@ import './ProductView.css'
 
 function ProductView({ products }) {
     // TODO: Replace with state variable
-    const [sideOpen, setSideOpen] = useState(true);
-    const [selectedProduct, setSelectedProduct] = useState('');
+    const sideOpenInit = localStorage.getItem("sideOpen") ? localStorage.getItem("sideOpen") : true
+    const selectedProductInit = localStorage.getItem("product") ? localStorage.getItem("product") : ""
+
+    const [sideOpen, setSideOpen] = useState(sideOpenInit);
+    const [selectedProduct, setSelectedProduct] = useState(selectedProductInit);
     useEffect(() => {
+        if (selectedProduct) {
         setSideOpen(true);
+        }
     }, [selectedProduct]);
 
+    useEffect(() => {
+        if (!sideOpen) setSelectedProduct("");
+    },[sideOpen])
+
+    useEffect(() => {
+            localStorage.setItem("product",selectedProduct)
+            localStorage.setItem("sideOpen",sideOpen)
+        
+})
     return (
         <div className="product-view">
             <div className="product-main-area">
